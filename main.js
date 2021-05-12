518c4ef57c76530161d78f252a7ee476ccf95bb6
Marina8989 <marina.mill209@gmail.com>
2021-05-12 12:37:28 -0700

    js interview QA

--- main.js
+++ main.js
@@ -2,6 +2,25 @@
 //console.log(2 + '2'); //=> 22
 //console.log(2 - '2'); //=> 0
 
-//2. remove dublicate number from an array, but you are not allowd to use any loops, maps etc.
+//2. remove duplicate number from an array, but you are not allowd to use any loops, maps etc.
 
-let nums = [1,2,2,3]; // => should return [1,2,3]
+// let nums = [1,2,2,3]; // => should return [1,2,3]
+// let uniqueNums = [... new Set(nums)];
+// console.log(uniqueNums);
+
+
+//3.make sur that var also becomes a block scoped like let
+
+let func = function() {
+  
+    {
+        (function() {
+           let l = 'let'
+           var v = 'var'
+        })(); 
+    }
+
+    console.log(l);
+    console.log(v);
+}
+func();
