0bfad455af1a4766e218a2a42d162e9a4acace77
Marina8989 <marina.mill209@gmail.com>
2021-05-12 12:22:26 -0700

    js interview practice

---
+++ main.js
@@ -0,0 +1,7 @@
+//1. what is the result?
+//console.log(2 + '2'); //=> 22
+//console.log(2 - '2'); //=> 0
+
+//2. remove dublicate number from an array, but you are not allowd to use any loops, maps etc.
+
+let nums = [1,2,2,3]; // => should return [1,2,3]
