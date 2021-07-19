f6a707d510eb744bbb04dd4af75cb7b62d71b1c1
Marina8989 <marina.mill209@gmail.com>
2021-07-18 17:40:23 -0700

    js algorithms

--- main.js
+++ main.js
@@ -285,16 +285,32 @@
 
 
 
-function powersOfTwo(n){
-  let arr = [];
-  let num = 1;
-   for(let i = 1; i <= n; i++) {
-      arr.push(num)
-      num += num;
-   } 
-   arr.push(num)
-   return arr
+// function powersOfTwo(n){
+//   let arr = [];
+//   let num = 1;
+//    for(let i = 1; i <= n; i++) {
+//       arr.push(num);
+//       num += num;
+//    } 
+//    arr.push(num);
+//    return arr;
+// }
+
+// console.log(powersOfTwo(4))// [1, 2, 4, 8, 16]
+
+function multiplesOf3and5(number) {
+   let sum = 0;
+   for(let i = 0; i < number; i++) {
+      if(i % 5 == 0 || i % 3 == 0 ) {
+        sum += i;
+     }
+   }
+   return sum;
 }
 
-console.log(powersOfTwo(4))// [1, 2, 4, 8, 16]
+console.log(multiplesOf3and5(1000));
 
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
