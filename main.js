8a9ef8363d7b14b0552df265c5b281e322f92caf
Marina8989 <marina.mill209@gmail.com>
2021-11-29 17:47:01 -0800

    js

--- main.js
+++ main.js
@@ -734,19 +734,34 @@
 // console.log(makeArrayConsecutive2([0, 3])) //2
 
 
-function almostIncreasingSequence(sequence) {
-    const sorted = sequence.sort((a,b) => {
-      return a - b;
-    })
-    console.log(sorted)
+// function almostIncreasingSequence(sequence) {
+//     const sorted = sequence.sort((a,b) => {
+//       return a - b;
+//     })
+//     console.log(sorted)
 
-    if(sorted === sequence) {
-       console.log(true)
-    }else {
-       console.log(false)
+//     if(sorted === sequence) {
+//        console.log(true)
+//     }else {
+//        console.log(false)
+//     }
+    
+// }
+
+// console.log(almostIncreasingSequence([1, 3, 2, 1]))// false
+// console.log(almostIncreasingSequence([1, 3, 2])) // true
+
+
+
+function solution(sequence){
+  let count = 0;
+    for(let i = 0; i < sequence.length; i++){
+        if (sequence[i] <= sequence[i-1]) count++;
+        if (sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) count++;
     }
     
-}
+    return count < 2;
+};
 
-console.log(almostIncreasingSequence([1, 3, 2, 1]))// false
-console.log(almostIncreasingSequence([1, 3, 2])) // true
+console.log(solution([1,3,2,1]))
+console.log(solution([1,3,2]))
